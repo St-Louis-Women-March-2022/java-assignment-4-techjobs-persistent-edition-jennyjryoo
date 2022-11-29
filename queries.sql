b2ec9a10fba2ca4836401b2c4da397333966b372
jennyryoo <jennyjryoo@gmail.com>
2022-11-29 14:51:44 -0800

    Adjusted autograder task 4

--- queries.sql
+++ queries.sql
@@ -8,4 +8,6 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
-SELECT name FROM job_skills INNER JOIN skill ON skill.id = job_skills.skills_id;
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
